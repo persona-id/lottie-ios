6553bdfdfe98f82b4a069038abc08095c894312d
Jacob Lange <jacob@withpersona.com>
2024-07-25 13:54:02 -0400

    chore: namespace to avoid duplicate symbols

--- Package.swift
+++ Package.swift
@@ -2,16 +2,16 @@
 import PackageDescription
 
 let package = Package(
-  name: "Lottie",
+  name: "PersonaLottie2",
   // Minimum platform versions should be kept in sync with the per-platform targets in Lottie.xcodeproj, lottie-ios.podspec, and lottie-spm's Package.swift
   platforms: [.iOS("13.0"), .macOS("10.15"), .tvOS("13.0"), .custom("visionOS", versionString: "1.0")],
-  products: [.library(name: "Lottie", targets: ["Lottie"])],
-  dependencies: [
-    .package(url: "https://github.com/airbnb/swift", .upToNextMajor(from: "1.0.1")),
-  ],
+  products: [.library(name: "PersonaLottie2", targets: ["PersonaLottie2"])],
+  // dependencies: [
+  //  .package(url: "https://github.com/airbnb/swift", .upToNextMajor(from: "1.0.1")),
+  // ],
   targets: [
     .target(
-      name: "Lottie",
+      name: "PersonaLottie2",
       path: "Sources",
       exclude: [
         "Private/EmbeddedLibraries/README.md",
